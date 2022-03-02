50c6bc72ca362c9ffd9ee23f09d23663e9eefb4a
m_tanaka <m_tanaka@MacBook-Pro.local>
2022-03-03 07:03:19 +0900

    画像細分化してNMF は時間かかりすぎて現実的じゃないわ

--- minting_product/minting_product.py
+++ minting_product/minting_product.py
@@ -5,13 +5,20 @@ import os
 import pickle
 import time
 import cv2
+import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import RidgeClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import RidgeClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.image import extract_patches_2d as patches
+from sklearn.feature_extraction.image import reconstruct_from_patches_2d
+
+from sklearn.decomposition import NMF
 
 
 # 提出ファイル作成
@@ -52,6 +59,26 @@ def open_image(file_name, is_train_data):
     return np.array(Image.open(os.getcwd() + '/datas/' + file_name_path))
 
 
+def load_patched_image_data(max_number=10, is_train_data=True):
+    images = load_image_datas(max_number, is_train_data=is_train_data)
+    patched_images = []
+
+    for i in range(max_number):
+        pre_patch = images[i].reshape(-1, 300)
+        patched_image = patches(pre_patch, (30, 30))
+        hoge = patched_image.reshape(-1, 1).shape
+        huga2 = patched_image.reshape(-1, 1).transpose().shape
+        print(huga2)
+        continue
+        patched_images.append(patched_image.reshape(-1, 1).shape)
+    return np.asarray(patched_images)
+    # 細分化した画像の復元確認用。
+    # reconstructed = reconstruct_from_patches_2d(patched_images, pre_patch.shape)
+    # print(patched_images.shape)
+    # print(reconstructed.shape)
+    # Image._show(Image.fromarray(reconstructed))
+
+
 def load_image_datas(max_number, is_train_data=True):
     pixel = 300*300
     env = 'train_data_' if is_train_data else 'test_data_'
@@ -64,11 +91,11 @@ def load_image_datas(max_number, is_train_data=True):
         return
 
     for num in range(max_number):
-        file_name = env + str(num + 1) + '.jpeg'
+        file_name = env + str(num + 1) + '.jpg'
         img = open_image(file_name, is_train_data)
         # グレースケール化
         gray_scale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        canny = cv2.Canny(gray_scale, 100, 200)
+        # canny = cv2.Canny(gray_scale, 100, 200)
         images_list.append(gray_scale)
 
 
@@ -78,17 +105,18 @@ def load_image_datas(max_number, is_train_data=True):
 
 def classifier_number_from_images(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, stratify=y)
-    # estimators = [RidgeClassifier()]
-    estimators = [SVC()]
-
+    # estimators = [MLPClassifier(max_iter=5000)]
+    estimators = [GaussianNB(), SVC(), RidgeClassifier(), RandomForestClassifier()]
 
     # return
     print('全データ数 = {}'.format(X.shape[0]))
     for estimator in estimators:
+        start_time = time.time()
         print('推定器 =           {}'.format(estimator.__class__))
         pipe = make_image_pipe_line(estimator)
         print('結果(推定器単体) =    {:.2f}'.format(estimator.fit(X_train, y_train).score(X_test, y_test)))
         print('結果(パイプ) =       {:.2f}'.format(pipe.fit(X_train, y_train).score(X_test, y_test)))
+        print("学習時間 = {:.2f}秒".format(time.time() - start_time))
         print('--------')
 
 
@@ -105,34 +133,47 @@ def estimate_image(data_count):
 
 
 def main():
-    train_data_count = 4400
+    train_data_count = 5
 
-    # # 訓練用
-    # # 画像データ読み込み
-    # train_image_datas = load_image_datas(train_data_count)
-    # reshaped_data = train_image_datas.reshape(-1, 90000)
-    #
-    # # 正解データの読み込み
-    # num_pd = read_pd_data('train_aug.csv')
-    # y = num_pd[:train_data_count]['target'].values
-    #
-    # # # 予測してどの推定器が一番良さげか。
-    # classifier_number_from_images(reshaped_data, y)
-    #
-    # # 学習済みモデルの作成
-    # # fitted_model = fit_image_model(reshaped_data, y, MLPClassifier())
-    # # write_fitted_model(fitted_model)
+    # 訓練用
+    # 画像データ読み込み
 
+    raw_images = load_image_datas(train_data_count)
+    print('raw_images.shape= {}'.format(raw_images.shape))
+    print('raw_images[0].shape= {}'.format(raw_images[0].shape))
 
-    # 本番用
-    test_data_count = 100
-    test_image_datas = load_image_datas(test_data_count, is_train_data=False)
-    test_reshaped_data = test_image_datas.reshape(-1, 90000)
+    train_image_datas = load_patched_image_data(max_number=5)
+    print('train_image_datas.shape= {}'.format(train_image_datas.shape))
+    print('train_image_datas[0].shape= {}'.format(train_image_datas[0].shape))
+    print('train_image_datas[0].reshape(-1,1).shape= {}'.format(train_image_datas[0].reshape(-1, 1).shape))
+    return
 
+    # 正解データの読み込み
+    num_pd = read_pd_data('train_aug.csv')
+    y = num_pd[:train_data_count]['target'].values
+
+    nmf = NMF(n_components=30, init='random', random_state=0)
+    nmf.fit(train_image_datas)
+
+    return
+
+    # # 予測してどの推定器が一番良さげか。
+    classifier_number_from_images(reshaped_data, y)
 
     # 学習済みモデルの作成
-    fitted_model = load_fitted_model("fitted_model_svc.pickle")
-    output_submit(test_reshaped_data, fitted_model)
+    # fitted_model = fit_image_model(reshaped_data, y, MLPClassifier())
+    # write_fitted_model(fitted_model)
+
+
+    # # 本番用
+    # test_data_count = 100
+    # test_image_datas = load_image_datas(test_data_count, is_train_data=False)
+    # test_reshaped_data = test_image_datas.reshape(-1, 90000)
+    #
+    #
+    # # 学習済みモデルの作成
+    # fitted_model = load_fitted_model("fitted_model_svc.pickle")
+    # output_submit(test_reshaped_data, fitted_model)
     print("end.")
 
 main()
