32975ac7593235e217395fa84c14fa2d793a57e9
m_tanaka <m_tanaka@MacBook-Pro.local>
2022-03-07 06:50:11 +0900

    なぜすいていしたこすうがちがうのかなぜか

--- koukoku/koukoku.py
+++ koukoku/koukoku.py
@@ -3,6 +3,11 @@ import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
 # from sklearn.model_selection import cross_validate
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss
@@ -12,6 +17,7 @@ import os
 
 def file_path(file_name):
     return 'koukoku' + '/' + file_name
+
 
 # 提出ファイル作成
 def output_submit(result, file_name='submit_blank.csv'):
@@ -24,9 +30,15 @@ def read_pd_data(file_name, headerFlag="infer"):
     return pd.read_table(os.getcwd() + '/datas/' + file_name, index_col=0, header=headerFlag)
 
 
-
 def to_dummies(data, column_name):
     return pd.get_dummies(data=data, columns=column_name)
+
+
+def pipe_line(estimator):
+    return Pipeline([('StandardScaler', StandardScaler()),
+                     ('Estimator', estimator)
+                     ])
+
 
 def model_pred_pd_data(X, y, estimator):
     if not all([hasattr(estimator, 'fit'), hasattr(estimator, 'predict'), hasattr(estimator, 'score')]):
@@ -34,43 +46,81 @@ def model_pred_pd_data(X, y, estimator):
         return
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0, stratify=y)
-    logloss = log_loss(estimator.fit(X_train, y_train).predict(X_test), y_test, labels=[0, 1])
+
+    logloss_estimate = log_loss(y_test, estimator.fit(X_train, y_train).predict_proba(X_test))
+    logloss_pipe = log_loss(y_test, pipe_line(estimator).fit(X_train, y_train).predict_proba(X_test))
 
     print('estimator = {}'.format(estimator.__class__))
-    print('結果(推定器単体) =    {:.2f}'.format(logloss))
+    # print('結果(推定器単体) =    {:.2f}'.format(logloss_estimate))
+    print('結果(パイプ) =    {:.2f}'.format(logloss_pipe))
     # print('前処理なし = {}'.format(cross_validate(estimator, X, y)["test_score"].mean()))
     print('----------')
 
+
 def choose_candiate(X, y):
     # MLPClassifier()は実行コストがかかりすぎる & 精度そんなによくないので除外
-    estimators = [GaussianNB(),
-                  LogisticRegression(max_iter=1000)]
+    # estimators = [GaussianNB(),
+    #               LogisticRegression(max_iter=1000)]
+    estimators = [LogisticRegression(max_iter=1000)]
 
     for estimator in estimators:
-         model_pred_pd_data(X.values, y.values, estimator)
+        model_pred_pd_data(X.values, y.values, estimator)
+
 
 def main():
+    pd.set_option('display.max_columns', 100)
+    pd.options.display.precision = 2
+    pd.options.display.float_format = '{:.2f}'.format
 
-    # train_data = read_pd_data("train_sample.tsv")
     train_data = read_pd_data("train.tsv")
+    # train_data = read_pd_data("train_sample.tsv")
+    # 欠損値どうしようか。
+    train_data = train_data.fillna(0)
 
     # これはターゲットなので、必ず drop。
     X = train_data.drop('click', axis=1)
 
     # とりあえず他の要素と相関係数が 0.7以上ある項目は drop してみる。
-    X = train_data.drop(columns=['C1', 'I7'], axis=1)
 
-    dummy_data = to_dummies(train_data, ['I1'])
+    # dummy_data = to_dummies(train_data, ['I1', 'I3', 'I4', 'I6', 'I7', 'I8', 'I9', 'I10'])
 
+    y = train_data['click']
+
+    # 推定器をいい感じに調べる
+    # choose_candiate(X, y)
+
+
+def testtest():
+    pd.set_option('display.max_columns', 100)
+    pd.options.display.precision = 2
+    pd.options.display.float_format = '{:.2f}'.format
+
+    train_data = read_pd_data("train.tsv")
+    # train_data = read_pd_data("train_sample.tsv")
     # 欠損値どうしようか。
-    X = X.fillna(0)
-    # print(X)
+    train_data = train_data.fillna(0)
 
-    y = dummy_data['click']
+    # これはターゲットなので、必ず drop。
+    X = train_data.drop('click', axis=1)
 
-    choose_candiate(X, y)
+    # とりあえず他の要素と相関係数が 0.7以上ある項目は drop してみる。
 
+    # dummy_data = to_dummies(train_data, ['I1', 'I3', 'I4', 'I6', 'I7', 'I8', 'I9', 'I10'])
 
-main()
+    y = train_data['click']
 
+    # 推定器をいい感じに調べる
+    # choose_candiate(X, y)
 
+    test_data = read_pd_data("test.tsv")
+    X_test = test_data.fillna(0)
+
+    sample_submit = read_pd_data('sample_submit.csv')
+    logloss_pipe = pipe_line(LogisticRegression(max_iter=1000)).fit(X, y).predict_proba(X_test)
+
+    sample_submit[1] = logloss_pipe[:, 1]
+    sample_submit[1] = sample_submit[1].round(2)
+    print(sample_submit[1].round(2))
+    sample_submit.to_csv('submit2.tsv', header=None, sep='\t')
+
+testtest()
